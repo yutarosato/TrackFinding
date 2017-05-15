9b5d2d70a1fd9ffe382b8b4d107eba08ec13df5a
Yutaro Sato <syutaro@post.kek.jp>
2017-05-16 08:21:55 +0900

    mod

--- ana/tracking.cpp
+++ ana/tracking.cpp
@@ -1,7 +1,7 @@
 #include "setting.h"
 
 const Int_t    fl_message         = 2; // 2(debug), 1(normal), 0(silent)
-const Int_t    fl_show            = 0;
+const Int_t    fl_show            = 1000;
 const Double_t th_show_energy_min = 150.0;
 const Double_t th_show_energy_max = 400.0;
 const Int_t    threshold_success  = 3; // Hit definition : >= threshold_success/range_success
